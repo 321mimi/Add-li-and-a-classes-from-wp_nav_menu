d267b681c0fce9df67110ca3214a8a6fb9911735
Michelle Cantin <contact@michellecantin.ca>
2019-08-17 16:38:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Add li and a classes from wp_nav_menu
+ Snippet that allows you to add classes to li and a tag from wp_nav_menu.
---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Functions.php
+ *
+ *      Map: Register your menu
+ *          Adds class to link automatically
+ *          Adds class to li automatically
+ */
+
+/* Register your menu */
+function register_my_menus() {
+  register_nav_menus(
+    array(
+        'header-menu' => __('Header Menu')
+     )
+   );
+}
+add_action('init', 'register_my_menus');
+
+/* Adds class to link automatically */
+add_filter( 'nav_menu_link_attributes', function($atts, $item, $args) {
+	if (property_exists($args, 'link_class')) {
+    	$atts['class'] = $args->link_class;
+  	}
+  return $atts;
+}, 1, 3 );
+
+/* Adds class to li automatically */
+add_filter( 'nav_menu_css_class', function($classes, $item, $args) {
+	if (property_exists($args, 'li_class')) {
+		$classes[] = $args->li_class;
+	}
+    return $classes;
+}, 10, 4 );
